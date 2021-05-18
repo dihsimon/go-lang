3081281f737008c8b0c93ac03de735bb7f0d6ea0
Diego Simon Carmona <diegosimoncarmona@gmail.com>
2021-05-18 20:13:46 -0300

    finalizando a função do fibonacci

--- 14 - Funções avançadas/14.3 - Funções Recursivas/recursivas.go
+++ 14 - Funções avançadas/14.3 - Funções Recursivas/recursivas.go
@@ -5,11 +5,18 @@ import (
 )
 
 func fibonacci(posicao uint) uint {
-
+	if posicao <= 1 {
+		return posicao
+	}
+	return fibonacci(posicao-2) + fibonacci(posicao-1)
 }
 
 func main() {
 	fmt.Println("Funções Recursivas")
 
-	//
+	posicao := uint(12)
+
+	for i := uint(1); i <= posicao; i++ {
+		fmt.Println(fibonacci(i))
+	}
 }
